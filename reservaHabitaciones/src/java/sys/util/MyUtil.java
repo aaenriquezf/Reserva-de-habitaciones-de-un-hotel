218b6fe1a017c26f641dcd573e24df920c8993be
aaenriquezf <aracely.enriquez@udla.edu.ec>
2018-06-02 08:11:53 -0500

    Rutas de direccionamiento utilizadas para el proyecto

---
+++ reservaHabitaciones/src/java/sys/util/MyUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sys.util;
+
+/**
+ *
+ * @author lude
+ */
+public class MyUtil {
+    
+    public static String baseurl()
+    {
+        return "http://localhost:8080/reservaHabitaciones/";
+    }
+
+    public static String basepathlogin()
+    {
+        return "/reservaHabitaciones/faces/Views/";
+    }
+    
+    public static String basepath()
+    {
+        return "/faces/Views/";
+    }
+}
